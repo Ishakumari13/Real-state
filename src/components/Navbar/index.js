9cf9d43f7f19ccdd9272bf19c423b4f2b2d512a5
Kumari <isha.a.kumari@accenture.com>
2022-12-01 14:29:57 +0530

    code commit

---
+++ src/components/Navbar/index.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import {
+Nav,
+NavLink,
+Bars,
+NavMenu,
+NavBtn,
+NavBtnLink,
+} from './NavbarElements';
+
+const Navbar = () => {
+return (
+	<>
+	<Nav>
+		<Bars />
+
+		<NavMenu>
+		<div className='logo'>
+            <h2>
+                <span>M</span>agic
+                <span>B</span>ricks
+            </h2>
+
+        </div>
+		<NavLink to='/about' activeStyle>
+			About
+		</NavLink>
+		
+		<NavLink to='/sign-up' activeStyle>
+			Sign Up
+		</NavLink>
+		{/* Second Nav */}
+		{/* <NavBtnLink to='/sign-in'>Sign In</NavBtnLink> */}
+		</NavMenu>
+		<NavBtn>
+		<NavBtnLink to='/signin'>Sign In</NavBtnLink>
+		</NavBtn>
+	</Nav>
+	</>
+);
+};
+
+export default Navbar;
